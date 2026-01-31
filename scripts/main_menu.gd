e764f624b4ce9261087d920a0762d19d4bdbe72f
Roukurai <roukurai@kuraitachi.com>
2026-01-31 12:24:17 -0600

    init main menu and placeholder assets for demo

---
+++ scripts/main_menu.gd
@@ -0,0 +1,23 @@
+extends MarginContainer
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_start_button_up() -> void:
+	get_tree().change_scene_to_file("res://scenes/loop.tscn")
+
+
+func _on_exit_button_up() -> void:
+	get_tree().quit()
+
+
+func _on_about_button_up() -> void:
+	pass # Replace with function body.
