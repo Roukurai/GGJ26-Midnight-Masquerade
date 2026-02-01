60693ad73af5d1b12236540eef6a4f757b69e6f2
Andres Hernandez <randresho@gmail.com>
2026-02-01 15:00:13 -0600

    ADD waves system

--- scripts/loop.gd
+++ scripts/loop.gd
@@ -1,8 +1,10 @@
 extends Node2D
+class_name loopGame
 
 const PLAYER: PackedScene = preload('res://scenes/player.tscn')
 var TOTEM: Node2D
 @export var music: MusicController
+@export var enemies: EnemiesController
 
 func _ready() -> void:
 	TOTEM = get_tree().get_first_node_in_group("Totem")
@@ -16,7 +18,6 @@ func _ready() -> void:
 	add_child(player)
 	music.enter_map()
 
-
 	
 func on_wave_started():
 	music.enemies_appear()
@@ -24,7 +25,7 @@ func on_wave_started():
 func on_wave_escalation():
 	music.enemy_next_layer()
 
-func on_wave_cleared():
+func on_wave_cleared():	
 	music.enemies_defeated()
 	
 func _end_game():
