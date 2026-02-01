99ab9dacf383c9024be4868553442ae1f23d38a4
Roukurai <roukurai@kuraitachi.com>
2026-02-01 14:57:18 -0600

    changed player skin, added projectile and mask mapping for buff effects

--- scripts/mask.gd
+++ scripts/mask.gd
@@ -1,22 +1,53 @@
 extends Area2D
+class_name MaskPickup
 
-## The type of mask this pickup grants. Uses Player.ProjectileType enum.
-@export var mask_type: Player.ProjectileType = Player.ProjectileType.BASIC
+@onready var sprite: Sprite2D = $Sprite2D
 
+# CHANGE / ADD MASKS HERE ONLY
+const MASKS := [
+	{
+		"id": "white",
+		"region": Rect2(35, 0, 32, 32),
+		"projectile_type": "neutral",
+		"damage": 1,
+		"fire_rate": 0.25,
+		"projectile_speed": 300,
+		"sprite_index": 0,
+		"effect": "NONE"
+	},
+	{
+		"id": "fire",
+		"region": Rect2(0, 79, 32, 32),
+		"projectile_type": "fire",
+		"damage": 2,
+		"fire_rate": 0.3,
+		"projectile_speed": 350,
+		"sprite_index": 1,
+		"effect": "DOT"
+	},
+	{
+		"id": "ice",
+		"region": Rect2(35, 79, 32, 32),
+		"projectile_type": "ice",
+		"damage": 1,
+		"fire_rate": 0.35,
+		"projectile_speed": 400,
+		"sprite_index": 2,
+		"effect": "SLOW"
+	}
+]
 
-# Called when the node enters the scene tree for the first time.
+var mask_data: Dictionary
+
 func _ready() -> void:
+	mask_data = MASKS.pick_random()
+
+	sprite.region_enabled = true
+	sprite.region_rect = mask_data.region
+
 	body_entered.connect(_on_body_entered)
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
-
-# Called when a physics body enters this Area2D's collision shape.
 func _on_body_entered(body: Node) -> void:
 	if body is Player:
-		body.player_data["mask_type"] = mask_type
-		print("DEBUG: Player picked up mask type: ", mask_type)
-		queue_free()  # Remove mask after pickup
+		body.equip_mask(mask_data)
+		queue_free()
